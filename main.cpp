9c3ce904f6e52e5adbc94c9e1dfdba17ada6daf3
Alih5690000 <ffffffaaaaaa320@outlook.com>
2026-04-28 12:26:15 +0000

    pls finish Player::isMated

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <windows.h>
 #include <vector>
+#include <utility>
+#include <cmath>
 
 class Piece{
     public:
@@ -16,9 +18,9 @@ class Piece{
     }
     Piece(int x,int y, std::vector<Piece*>& all_pieces):x(x),y(y),all_pieces(all_pieces){}
     virtual ~Piece()=default;
-    virtual bool islegal(){return true;}
+    virtual bool islegal(int dx,int dy){return true;}
     int move(int x,int y){
-        if(islegal()){
+        if(islegal(x,y)){
             for (auto i:all_pieces){
                 if (i->x==x && i->y==y){
                     take(i);
@@ -33,8 +35,24 @@ class Piece{
             return -1;
         }
     }
+    [[maybe_unused]] std::vector<std::pair<int,int>> possibleMoves(){
+        return {{}};
+    }
     void draw(char map[8][8]){
         map[y][x]='$';
+    }
+};
+
+class King:public Piece{
+    public:
+    King(int x, int y, std::vector<Piece*>& p):Piece(x,y,p){}
+    bool islegal(int dx, int dy) override{
+        int X=(dx-x)*(dx-x);
+        int Y=(dy-y)*(dy-y);
+        if (X!=1 || Y!=1)
+            return false;
+        else
+            return true;
     }
 };
 
@@ -59,6 +77,31 @@ void init(){
     GetConsoleMode(hOut, &mode);
     SetConsoleMode(hOut, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
 }
+
+class Player{
+    public:
+    King* king;
+    std::vector<Piece*>& all_pieces;
+    std::vector<Piece*> pieces;
+    Player(std::vector<Piece*>& ap):all_pieces(ap){
+        king=new King(0,0,all_pieces);
+    }
+    bool isMated(){//finish it}
+    int DoMove(int x, int y,int dx, int dy){
+        for (auto i:pieces){
+            if (i->x==x && i->y==y){
+                if (i->islegal(dx,dy)){
+                    i->move(dx,dy);
+                    return 0;
+                }
+                else{
+                    return -1;
+                }
+            }
+        }
+        return 1;
+    }
+};
 
 int main(){
     char map[8][8];
